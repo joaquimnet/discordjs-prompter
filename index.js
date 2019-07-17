8ef5f9c8a1a7097bcd7fa7dfa3ce75057dd6b88c
Popon <joaquimmy@yahoo.com>
2019-07-17 09:35:46 -0300

    🚚 Moved index.js into src folder

--- index.js
+++
@@ -1,4 +0,0 @@
-const message = require('./src/message');
-const reaction = require('./src/reaction');
-
-module.exports = { message, reaction };
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const message = require('./message');
+const reaction = require('./reaction');
+
+module.exports = { message, reaction };
