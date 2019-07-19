6f63c41a75700d3956f3244591e64be679b8ef17
Popon <joaquimmy@yahoo.com>
2019-07-19 14:03:27 -0300

    📝 Generated docs

---
+++ docs/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"32":"Variable","2097152":"Object literal"},"rows":[{"id":0,"kind":2097152,"name":"Prompter","url":"globals.html#prompter","classes":"tsd-kind-object-literal"},{"id":1,"kind":32,"name":"message","url":"globals.html#prompter.message","classes":"tsd-kind-variable tsd-parent-kind-object-literal","parent":"Prompter"},{"id":2,"kind":32,"name":"reaction","url":"globals.html#prompter.reaction","classes":"tsd-kind-variable tsd-parent-kind-object-literal","parent":"Prompter"},{"id":3,"kind":32,"name":"vote","url":"globals.html#prompter.vote","classes":"tsd-kind-variable tsd-parent-kind-object-literal","parent":"Prompter"}]};
